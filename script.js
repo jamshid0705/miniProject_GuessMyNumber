c649911a1d7aa0e5baf09a6d5d06be966ef199d3
jamshid <jamshidshamshod0705@gmail.com>
2023-02-24 09:52:44 +0500

    event

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
-console.log(document.querySelector('.message').textContent)
+// console.log(document.querySelector('.message').textContent)
+
+// const a={
+//   a:'w',
+//   b:function(){
+//     return 2;
+//   }
+// }
+
+// a.b
+
+document.querySelector('.check').addEventListener('click',function(){
+
+})
+console.log(typeof document)
